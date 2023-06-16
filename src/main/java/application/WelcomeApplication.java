01a2efadf73e8b946a08d03ec99900ae96884a7f
david <davidelaureti54@gmail.com>
2023-06-16 19:21:46 +0200

    Added logging aspect

--- src/main/java/application/WelcomeApplication.java
+++ src/main/java/application/WelcomeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -24,6 +25,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EntityScan(basePackages = {
         "domain"
 })
+@EnableAspectJAutoProxy
 public class WelcomeApplication {
     public static void main(String[] args) {
         SpringApplication.run(WelcomeApplication.class, args);
---
+++ src/main/java/aspects/logging/LoggingAspect.java
@@ -0,0 +1,49 @@
+package aspects.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggingAspect {
+    @Pointcut("@annotation(Log)")
+    public void logAnnotation() {
+    }
+
+    @Pointcut("execution(* controller.*.*(..))")
+    public void controllerExecutions() {
+    }
+
+    @Before("logAnnotation()")
+    public void logAnnotationAdvice(JoinPoint joinPoint) {
+        log.info("@Log Method called: " + joinPoint.getSignature().getName());
+        log.info(createJoinPointForLogs(joinPoint));
+    }
+
+    @Before("controllerExecutions()")
+    public void controllerExecutionsAdvice(JoinPoint joinPoint) {
+        log.info("Controller methods, method called: " + joinPoint.getSignature().getName());
+        log.info(createJoinPointForLogs(joinPoint));
+    }
+
+    private String createJoinPointForLogs(JoinPoint joinPoint) {
+        if (joinPoint.getArgs().length < 1) {
+            return joinPoint.getSignature().getName().concat(" method don`t have parameters");
+        }
+        Object[] obj = joinPoint.getArgs();
+        StringBuilder requestValue = new StringBuilder();
+        Arrays.stream(obj).forEach(x -> {
+            requestValue.append("\r\n");
+            requestValue.append(x.toString());
+        });
+        requestValue.append("\r\n============= =======" + "====== =============");
+        return requestValue.toString();
+    }
+}
