bd995f22d01c328995e4ec06ede251b5fda37670
david <davidelaureti54@gmail.com>
2023-06-24 18:39:35 +0200

    search tour unit test

--- src/main/java/dto/SearchTourRequestDTO.java
+++ src/main/java/dto/SearchTourRequestDTO.java
@@ -1,15 +1,16 @@
 package dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.util.List;
 
-public record SearchTourRequestDTO(@NotNull Long cityId,
-                                   @NotNull Integer maxDuration,
-                                   @NotBlank String themeName,
-                                   List<@NotBlank String> tagNames,
+/**
+ * Here the validation isn't expressed because these filters are optional
+ */
+public record SearchTourRequestDTO(Long cityId,
+                                   Integer maxDuration,
+                                   String themeName,
+                                   List<String> tagNames,
                                    @Positive int pageNumber,
                                    @Positive int pageSize) {
 }
--- src/test/java/service/unittest/TourServiceUnitTest.java
+++ src/test/java/service/unittest/TourServiceUnitTest.java
@@ -5,6 +5,7 @@ import domain.softdeletable.SoftDeletable;
 import domain.softdeletable.SoftDelete;
 import dto.CityDTO;
 import dto.EditTourRequestDTO;
+import dto.SearchTourRequestDTO;
 import dto.TagRequestDTO;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,6 +14,10 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import repository.TourRepository;
 import service.TourService;
 
@@ -86,5 +91,34 @@ public class TourServiceUnitTest {
         assertNotNull(tourEdited.getTags().get(0));
         assertEquals(tourEdited.getTags().get(0).getId(), mockId);
         assertEquals(tourEdited.getTags().get(0).getName(), mockTagName);
+    }
+
+    @Test
+    public void deleteTourTest() {
+        Long mockId = 1L;
+        Mockito.doNothing().when(tourRepository).deleteById(anyLong());
+        assertDoesNotThrow(() -> tourService.delete(mockId));
+    }
+
+    @Test
+    public void searchTourTest() {
+        Tour tour = new Tour();
+        String mockTitle = "mockTitle";
+        tour.setTitle("mockTitle");
+        SoftDelete softDelete = new SoftDelete();
+        tour.setSoftDelete(softDelete);
+        Page<Tour> mockPage = new PageImpl<>(List.of(tour));
+        Mockito.when(tourRepository.findAll((Specification<Tour>) any(), (Pageable) any()))
+                .thenReturn(mockPage);
+        List<Tour> tours = tourService.search(new SearchTourRequestDTO(null,
+                null,
+                null,
+                null,
+                0,
+                10));
+        assertNotNull(tours);
+        assertEquals(tours.size(), 1);
+        assertNotNull(tours.get(0));
+        assertEquals(tours.get(0).getTitle(), mockTitle);
     }
 }
